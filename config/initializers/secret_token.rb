01f9efa49ed3928f020430503bce36dbf06e47ea
Dounan Hu <dounan1@gmail.com>
2013-05-02 04:27:14 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProductExchange::Application.config.secret_token = '15e547e1a8df74937470c9e21b1d9fdee13e3af91d1f8e359553499dd98533277bcc666bb0bece08a30cf36376be77d1bea6e4c4b9d8252a19f310cb3c31832b'
